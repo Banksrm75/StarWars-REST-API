32393c7ccfdc4dff4ae42a2c6863c25ff166821b
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-12-04 04:52:48 +0000

    Working on User_Favorites Table

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, Users, User_Favorites, Favorite_Characters, Favorite_Planets, Favorite_Vehicles, Characters, Planets, Vehicles
+from models import db, Users, Characters, Planets, Vehicles, User_Favorites
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -11,13 +11,10 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(Users, db.session))
-    admin.add_view(ModelView(User_Favorites, db.session))
-    admin.add_view(ModelView(Favorite_Characters, db.session))
-    admin.add_view(ModelView(Favorite_Planets, db.session))
-    admin.add_view(ModelView(Favorite_Vehicles, db.session))
     admin.add_view(ModelView(Characters, db.session))
     admin.add_view(ModelView(Planets, db.session))
     admin.add_view(ModelView(Vehicles, db.session))
+    admin.add_view(ModelView(User_Favorites, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
